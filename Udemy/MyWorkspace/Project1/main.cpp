1dcac2aaaa32989fef3f52c3d9a61dff46fc4c30
Animesh Paul <ani.nitmz@gmail.com>
2020-10-04 19:12:58 -0600

    Added size of datatypes and vars

---
+++ Udemy/MyWorkspace/Project1/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <climits> 
+using namespace std;
+
+int main() {
+    cout << "Size Information " << endl;
+    cout << "=========================="<< endl;
+    cout << "char: "<< sizeof(char) << " bytes" << endl;
+    cout << "int: "<< sizeof(int) << " bytes" << endl;
+    cout << "unsigned int: "<< sizeof(unsigned int) <<" bytes"<< endl;
+    cout << "short: "<< sizeof(short) << " bytes" << endl;
+    cout << "long: "<< sizeof(long) << " bytes" << endl;
+    cout << "long long: "<< sizeof(long long) << " bytes" <<endl;
+    cout << endl;
+    cout << "=========================="<< endl;
+    cout << "float: "<< sizeof(float) << " bytes" << endl;
+    cout << "double: "<< sizeof(double) << " bytes" << endl;
+    cout << "long double: "<< sizeof(long double) << " bytes" << endl;
+    cout << endl;
+    cout << "=========================="<< endl;
+    cout << "Minimum Values"<<endl;
+    cout << "char: "<< CHAR_MIN << endl;
+    cout << "int: "<< INT_MIN << endl;
+    cout << "short: "<< SHRT_MIN << endl;
+    cout << "long: "<< LONG_MIN << endl;
+    cout << "long long: "<< LLONG_MIN << endl;
+    cout << endl;
+    cout << "Maximum Values"<<endl;
+    cout << "char: "<< CHAR_MAX << endl;
+    cout << "int: "<< INT_MAX << endl;
+    cout << "short: "<< SHRT_MAX << endl;
+    cout << "long: "<< LONG_MAX << endl;
+    cout << "long long: "<< LLONG_MAX << endl;
+    cout << endl;
+    cout << "Using sizeof with var names" <<endl;
+    int age {21};
+    cout <<"\nage is "<<sizeof age<<" bytes";
+    double wage {22.24};
+    cout <<"\nwage is "<<sizeof wage<<" bytes";
+    cout<<endl<<"\n";
+    return 0;
+}  
---
+++ Udemy/MyWorkspace/Project2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using std::cout; //std is a C++ namespace
+using std::cin; //std is a C++ namespace
+using std::endl; //std is a C++ namespace
+
+int main() 
+{
+    cout << "My template" << endl;
+    cout << sizeof(int) << endl;
+    return 0;
+ }  
