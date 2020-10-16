93a99e7fbd6ca3850596d29c97b77ae8654e249d
Animesh Paul <ani.nitmz@gmail.com>
2020-10-15 22:55:12 -0600

    Added dynamic Memeory Allocation

--- Udemy/MyWorkspace/Pointers/main.cpp
+++ Udemy/MyWorkspace/Pointers/main.cpp
@@ -14,6 +14,28 @@ int main() {
     for (auto stooge: *vector_ptr) // *vector_ptr resolves to stooges
         cout << stooge << endl;
         
+    // Dynamic Memory Allocation 
+    int *int_ptr {nullptr};
+    int_ptr = new int; // Memory allocated from the heap
+    *int_ptr = 5;
+    cout << "Heap Memory Content: "<<*int_ptr<< endl;
+    delete int_ptr; // free allocated memory and release it back to heap
+    
+    int *array_ptr {nullptr};
+    size_t size {};
+    
+    cout << "Enter the size of the Array: ";
+    cin >> size;
+    
+    array_ptr = new int[size]; //Dynamically allocate required memory from heap
+    
+    if (array_ptr)
+        cout << "Memory Allocated at base address: "<< array_ptr << endl;
+    else
+        cout << "Memory Allocation failed";
+    
+    delete [] array_ptr;
+        
     cout << endl << endl;
     return 0;
 }  
