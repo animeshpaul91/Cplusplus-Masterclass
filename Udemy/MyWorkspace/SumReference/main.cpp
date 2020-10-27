d9aec28d4790a7f9b41472af441c9755791d8103
Animesh Paul <ani.nitmz@gmail.com>
2020-10-26 20:45:31 -0600

    Modified Sum Reference

--- Udemy/MyWorkspace/SumReference/main.cpp
+++ Udemy/MyWorkspace/SumReference/main.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int& sum(int a, int b)
+int &sum(int a, int b)
 {
   int *c = new int;
   *c = a + b;
   return *c;
 }
 
-// To execute C++, please define "int main()"
 int main() {
   int &s = sum(10, 20);
   cout << s << endl;
