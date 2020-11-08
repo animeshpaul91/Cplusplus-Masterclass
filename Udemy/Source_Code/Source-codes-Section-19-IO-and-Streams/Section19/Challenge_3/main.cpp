42049122c2e024ccc1b4fba3d390340654d98439
Animesh Paul <ani.nitmz@gmail.com>
2020-11-08 16:47:14 -0700

    Added Section-3 Challenge Solution

--- Udemy/Source_Code/Source-codes-Section-19-IO-and-Streams/Section19/Challenge_3/main.cpp
+++ Udemy/Source_Code/Source-codes-Section-19-IO-and-Streams/Section19/Challenge_3/main.cpp
@@ -2,12 +2,29 @@
 // Challenge 3
 // Word counter
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main() {
+    ifstream file {"../romeoandjuliet.txt"};
+    if (!file)
+        return 1;
     
+    string substring {}, word {};
+    size_t total_words {}, occurences {};
+    cout << "Enter the substring to search for: ";
+    getline(cin, substring);
+    while (file >> word)
+    {
+        total_words++;
+        if (word.find(substring) != string::npos)
+            occurences++;
+    }
     
+    cout << endl;
+    cout << total_words << " were searched" << endl;
+    cout << "The Substring "<<substring<<" was found "<<occurences<<" times";
     cout << endl;
     return 0;
 }
