bc3835ef273998bab23e93fc182212df19bc6ea9
Animesh Paul <ani.nitmz@gmail.com>
2020-11-08 18:27:17 -0700

    Added Section-4 Challenge Solution

--- Udemy/Source_Code/Source-codes-Section-19-IO-and-Streams/Section19/Challenge_4/main.cpp
+++ Udemy/Source_Code/Source-codes-Section-19-IO-and-Streams/Section19/Challenge_4/main.cpp
@@ -2,9 +2,32 @@
 // Challenge 4
 // Copy Romeo and Juliet with line numbers
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
 
 int main() {
+    ifstream inp_file {"../romeoandjuliet"};
+    ofstream out_file {"../romeoandjulietcopy"};
     
+    if (!inp_file || !out_file)
+        return 1;
+    
+    string line {};
+    size_t line_number {};
+    
+    while (getline(inp_file, line)) {
+        if (line == "")
+            out_file << endl;
+        else
+            out_file << setw(10) << left << ++line_number << line << endl;
+    }
+    
+    cout << "File Copied" << endl;
+    inp_file.close();
+    out_file.close();
     return 0;
 }
 
