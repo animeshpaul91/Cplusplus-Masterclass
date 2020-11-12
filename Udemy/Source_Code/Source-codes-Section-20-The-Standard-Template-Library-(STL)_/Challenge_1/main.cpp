3fe10a2ecfeef2d1635dee0c8f6c36d8df9b7091
Animesh Paul <ani.nitmz@gmail.com>
2020-11-11 21:05:51 -0700

    Added Challenge-1 Solution

--- Udemy/Source_Code/Source-codes-Section-20-The-Standard-Template-Library-(STL)_/Challenge_1/main.cpp
+++ Udemy/Source_Code/Source-codes-Section-20-The-Standard-Template-Library-(STL)_/Challenge_1/main.cpp
@@ -12,11 +12,27 @@ bool is_palindrome(const std::string& s)
 {
     // You must implement this function.
     // Since we are learning the STL - use a deque to solve the problem.
-    return false;
+    std::deque<char> d;
+    for(const char &c: s)
+        if (std::isalpha(c))
+            d.push_back(tolower(c));
+    
+    char front {}, back {};
+    while (d.size() > 1) {
+        front = d.front();
+        back = d.back();
+        if (front != back)
+            return false;
+        d.pop_front();
+        d.pop_back();
+    }
+    
+    return true;
 }
 
 int main()
 {
+    std::cout << std::boolalpha << std::endl;
     std::vector<std::string> test_strings{ "a", "aa", "aba", "abba", "abbcbba", "ab", "abc", "radar", "bob", "ana",
         "avid diva", "Amore, Roma", "A Toyota's a toyota", "A Santa at NASA", "C++",
         "A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!", "This is a palindrome", "palindrome" };
@@ -26,6 +42,7 @@ int main()
     for(const auto& s : test_strings) {
         std::cout << std::setw(8) << std::left << is_palindrome(s)  << s << std::endl;
     }
+    
     std::cout << std::endl;
     return 0;
 }
